bc97d6bcb61dc30c8e263ad16859c6b6e3d86147
efromvt <ethan.dickinson@gmail.com>
2023-05-08 19:45:05 -0400

    enable committing examples folder

---
+++ examples/community.py
@@ -0,0 +1,35 @@
+from trilogy_public_models import models
+from preql import Dialects
+from preql_nlp import build_query
+from preql.hooks.query_debugger import DebuggingHook
+from logging import StreamHandler, DEBUG
+from preql_nlp.constants import logger
+
+logger.setLevel(DEBUG)
+logger.addHandler(StreamHandler())
+
+
+# grab the model we want to parse
+environment = models["bigquery.stack_overflow"]
+
+# define a few new metrics
+environment.parse("auto question.answer.count <- count(answer.id) by question.id;")
+
+environment.parse("auto question.answer.count.avg <- answer.count/ question.count;")
+
+processed_query = build_query(
+    "How many questions are asked per year? order results by year desc",
+    environment,
+    debug=True,
+)
+
+for key in processed_query.output_columns:
+    print(key)
+
+executor = Dialects.BIGQUERY.default_executor(
+    environment=environment, hooks=[DebuggingHook()]
+)
+
+results = executor.execute_query(processed_query)
+for row in results:
+    print(row)

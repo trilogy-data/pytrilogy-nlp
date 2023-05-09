66119ce5d3bc0e4e6f29de15477b7eb74cd299cb
efromvt <ethan.dickinson@gmail.com>
2023-05-08 20:17:37 -0400

    community files

---
+++ examples/community_birth.py
@@ -0,0 +1,31 @@
+from trilogy_public_models import models
+from preql import Dialects
+from preql_nlp import build_query
+from preql.hooks.query_debugger import DebuggingHook
+from logging import StreamHandler, DEBUG
+from preql_nlp.constants import logger
+
+logger.setLevel(DEBUG)
+logger.addHandler(StreamHandler())
+
+
+# grab the model we want to parse
+environment = models["bigquery.usa_names"]
+
+
+processed_query = build_query(
+    "Most popular names in the state of vermont in 1990?",
+    environment,
+    debug=True,
+)
+
+for key in processed_query.output_columns:
+    print(key)
+
+executor = Dialects.BIGQUERY.default_executor(
+    environment=environment, hooks=[DebuggingHook()]
+)
+
+results = executor.execute_query(processed_query)
+for row in results:
+    print(row)

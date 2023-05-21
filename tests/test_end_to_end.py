95d44ad298c33c607e6fa498d5067707f0a8cc13
efromvt <ethan.dickinson@gmail.com>
2023-05-21 19:44:55 -0400

    remove test community dependency

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -1,24 +1,40 @@
-
-
-
-from trilogy_public_models import models
 from preql_nlp.main import parse_query
-from preql.core.models import Select
+from preql.core.models import Environment, Concept
+from preql.core.enums import Purpose, DataType
 
 
-def test_e2e_community_so(test_logger):
+def test_e2e_basic(test_logger):
     # grab the model we want to parse
-    environment = models["bigquery.stack_overflow"]
-    location =environment.concepts['user.location']
+    environment = Environment()
+    id = Concept(
+        name="id", namespace="user", datatype=DataType.INTEGER, purpose=Purpose.KEY
+    )
+    location = Concept(
+        name="location",
+        namespace="user",
+        datatype=DataType.STRING,
+        purpose=Purpose.PROPERTY,
+        keys=[id],
+    )
+    name = Concept(
+        name="name",
+        namespace="user",
+        datatype=DataType.STRING,
+        purpose=Purpose.PROPERTY,
+        keys=[id],
+    )
+
+    environment.add_concept(concept=id)
+    environment.add_concept(concept=location)
+    environment.add_concept(concept=name)
+
+    location = environment.concepts["user.location"]
 
     processed_query = parse_query(
         "Who is a user in germany?",
         environment,
         debug=True,
     )
-    for x in processed_query.selection:
-        print(x)
-        test_logger.info(x)
     assert location in [x.content for x in processed_query.selection]
     # assert processed_query.fil
     # print(render_query(processed_query))

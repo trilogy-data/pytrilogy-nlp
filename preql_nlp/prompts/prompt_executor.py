d8b6f1fae9a3b7f38c914ea94c572544f9b6fa65
efromvt <ethan.dickinson@gmail.com>
2023-05-21 22:34:56 -0400

    Add in auto-healing to prompts

--- preql_nlp/prompts/prompt_executor.py
+++ preql_nlp/prompts/prompt_executor.py
@@ -22,7 +22,7 @@ import os
 from jinja2 import FileSystemLoader, Environment, Template
 from os.path import dirname
 
-PROMPT_STOPWORD = '<EOM>'
+PROMPT_STOPWORD = "<EOM>"
 
 loader = FileSystemLoader(searchpath=dirname(__file__))
 templates = Environment(loader=loader)
@@ -52,28 +52,34 @@ class BasePreqlPromptCase(TemplatedPromptCase):
     parse_model: Type[BaseModel]
     template: Template
     stopword = PROMPT_STOPWORD
+
     def __init__(
         self,
         category: str,
         fail_on_parse_error: bool = True,
         evaluators: Optional[Union[Callable, List[Callable]]] = None,
     ):
-
         # this isn't actually the right way to pass through a stopword to the complete prompt
         # so we're splitting in the response
         # TODO: figure out how to do this when we figure out how to group prompts in one API call
-        super().__init__(category=category, evaluators=evaluators, prompt_executor_kwargs={'stopword':PROMPT_STOPWORD})
+        super().__init__(
+            category=category,
+            evaluators=evaluators,
+            prompt_executor_kwargs={"stopword": PROMPT_STOPWORD},
+        )
         self._prompt_hash = str(uuid.uuid4())
         self.parsed = None
         self.fail_on_parse_error = fail_on_parse_error
+        self.retry_prompt = "User: That response was incorrectly formatted. Please return the same content as the first response with formatting fixed (eg. valid JSON) to conform with original request. System:\n"
         self.stash: BaseCache = SqlliteCache()
 
     @classmethod
-    def parse_response(cls, response:str):
+    def parse_response(cls, response: str):
         return cls.parse_model.parse_raw(response.split(cls.stopword)[0])
 
     def prompt_executor(self):
         from langchain.chat_models import ChatOpenAI
+
         model_name = os.environ.get("OPENAI_MODEL") or "text-davinci-003"
         openai_api_key = os.environ.get("OPENAI_API_KEY")
         self.prompt_executor_kwargs = {"model_name": model_name}
@@ -95,17 +101,25 @@ class BasePreqlPromptCase(TemplatedPromptCase):
         return self.response
 
     def get_extra_template_context(self):
-        return {'stopword': self.stopword}
+        return {"stopword": self.stopword}
+
+    def rerun(self):
+        self.prompt = self.prompt + self.response + "\n" + self.retry_prompt
+        self.execute_prompt(self.prompt)
 
     def post_run(self):
         try:
             self.parsed = self.parse_response(self.response)
         except ValidationError as e:
-            print(self.render())
-            print("was unable to parse response using ", str(self.parse_model))
-            print(self.response)
-            if self.fail_on_parse_error:
-                raise e
+            self.rerun()
+            try:
+                self.parsed = self.parse_response(self.response)
+            except ValidationError:
+                print(self.render())
+                print("was unable to parse response using ", str(self.parse_model))
+                print(self.response)
+                if self.fail_on_parse_error:
+                    raise e
 
     def render(
         self,
@@ -142,17 +156,17 @@ class SemanticToTokensPromptCase(BasePreqlPromptCase):
         tokens: List[str],
         phrases: List[str],
         evaluators: Optional[Union[Callable, List[Callable]]] = None,
-    ):  
-        
+    ):
         self.tokens = sorted(tokens)
         # we need to ensure that we always have a list
         # for chat-gpt to understand the prompt properly
         # pad out the inputs with a random phrase
-        self.phrases = sorted(phrases + ['democratic elections'])
+        self.phrases = sorted(phrases + ["democratic elections"])
         super().__init__(category="semantic_to_tokens", evaluators=evaluators)
 
     def get_extra_template_context(self):
-        return {**super().get_extra_template_context(), 
+        return {
+            **super().get_extra_template_context(),
             "tokens": ", ".join([f'"{c}"' for c in self.tokens if c]),
             "phrase_str": ", ".join([f'"{c}"' for c in self.phrases]),
         }
@@ -176,7 +190,8 @@ class SelectionPromptCase(BasePreqlPromptCase):
         self.execution.score = None
 
     def get_extra_template_context(self):
-        return {**super().get_extra_template_context(), 
+        return {
+            **super().get_extra_template_context(),
             "concept_string": ", ".join([f'"{c}"' for c in self.concept_names]),
             "question": self.question,
         }
@@ -199,7 +214,8 @@ class FilterRefinementCase(BasePreqlPromptCase):
         super().__init__(evaluators=evaluators, category="filter_refinement")
 
     def get_extra_template_context(self):
-        return {**super().get_extra_template_context(), 
+        return {
+            **super().get_extra_template_context(),
             "values": ", ".join([f'"{x}"' for x in self.values]),
             "description": self.description,
         }
@@ -238,7 +254,7 @@ def log_prompt_info(prompt: TemplatedPromptCase, session_uuid: uuid.UUID):
 
 
 @overload
-def run_prompt( # type: ignore
+def run_prompt(  # type: ignore
     prompt: SelectionPromptCase,
     debug: bool = False,
     log_info: bool = True,
@@ -268,7 +284,7 @@ def run_prompt(  # type: ignore
 
 
 @overload
-def run_prompt( # type: ignore
+def run_prompt(  # type: ignore
     prompt: FilterRefinementCase,
     debug: bool = False,
     log_info: bool = True,

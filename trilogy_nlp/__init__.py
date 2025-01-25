6a8a1890c44a43e197d6cb9e670a9c5cf00a3cb6
efromvt <ethan.dickinson@gmail.com.com>
2025-01-25 16:32:07 -0500

    version bump

--- trilogy_nlp/__init__.py
+++ trilogy_nlp/__init__.py
@@ -6,6 +6,6 @@ from .core import NLPEngine  # noqa: E402
 from .enums import CacheType, Provider  # noqa: E402
 from .main import build_query  # noqa: E402
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 __all__ = ["build_query", "Provider", "NLPEngine", "CacheType"]
